774da49dc6b645281a64a2a9040b3d2b44388c35
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-07-08 20:24:03 -0500

    fixed post request, had to fix the header and add an ID. make sure to ask about needing an ID

--- server/modules/routes/post-review.js
+++ server/modules/routes/post-review.js
@@ -1,18 +1,26 @@
 const pool = require("../pool.js");
 
-const QUERY = `INSERT INTO feedback VALUES ($1, $2, $3, $4);`;
+const QUERY = `INSERT INTO feedback VALUES ($1, $2, $3, $4, $5);`;
+let count = 1;
 
 function postReview(req, res) {
-  console.log('BODY -------- :', req.body)
-  const [feeling, understanding, support, comments] = req.body;
-  pool.query(QUERY, [feeling, understanding, support, comments])
-  .then((result) => {
-    res.sendStatus(201);
-  })
-  .catch((error) => {
-    console.error(error);
-    res.sendStatus(500)
-  })
+  console.log("BODY -------- :", req.body);
+  const review = req.body;
+  pool
+    .query(QUERY, [
+      count + 1,
+      review.feeling,
+      review.understanding,
+      review.support,
+      review.comments,
+    ])
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
 }
 
 module.exports = postReview;
--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -8,15 +8,18 @@ export function ReviewPage() {
   const history = useHistory();
 
   const nextPage = () => {
+    console.log("REVIEW --> ", review);
     fetch("/review", {
       method: "POST",
       body: JSON.stringify(review),
-      headers: { "Content-Type": "application, json" },
-    }).then((response) => {
-      history.push(`${pageNum + 1}`);
-    }).catch((error) => {
-      console.error(error);
+      headers: { "Content-Type": "application/json" },
     })
+      .then((response) => {
+        history.push(`${pageNum + 1}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
     dispatch({ type: `NEXT_PAGE` });
   };
 
