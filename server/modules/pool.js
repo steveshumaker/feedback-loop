021aff7c63d04b82a80420fa5a440b93711bef19
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-07-10 22:29:58 -0500

    adjusted env variable for heroku postgres

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,28 +1,28 @@
-const pg = require('pg');
+const pg = require("pg");
 let pool;
 
-// When our app is deployed to the internet 
+// When our app is deployed to the internet
 // we'll use the DATABASE_URL environment variable
 // to set the connection info: web address, username/password, db name
-// eg: 
+// eg:
 //  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
 if (process.env.DATABASE_URL) {
-    pool = new pg.Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
+  pool = new pg.Pool({
+    connectionString: `postgres://myzbmiujljjcrx:f47c4591cb98daba1a33a081d0de0e05490a71a984cff5d05739fe1b0d715558@ec2-3-217-146-37.compute-1.amazonaws.com:5432/dcprn97d4jsnge`,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 }
 // When we're running this app on our own computer
-// we'll connect to the postgres database that is 
+// we'll connect to the postgres database that is
 // also running on our computer (localhost)
 else {
-    pool = new pg.Pool({
-        host: 'localhost',
-        port: 5432,
-        database: 'prime_feedback', 
-    });
+  pool = new pg.Pool({
+    host: "localhost",
+    port: 5432,
+    database: "prime_feedback",
+  });
 }
 
 module.exports = pool;
