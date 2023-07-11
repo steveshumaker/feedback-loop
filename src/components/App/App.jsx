f4e40dacbe7e5f716d16fe4b328e7b9245bc67a4
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-07-10 22:53:31 -0500

    swapped to hashrouter to hopefully fix heroku routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 import { FeelingsPage } from "../FeelingsPage/FeelingsPage";
